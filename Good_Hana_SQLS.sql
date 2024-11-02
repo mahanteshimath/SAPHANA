1d057eb263de6ef90b0b7bd8bce6f68b03cd07b1
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-02 13:27:07 +0530

    Update Good_Hana_SQLS.sql

--- Good_Hana_SQLS.sql
+++ Good_Hana_SQLS.sql
@@ -29,3 +29,24 @@ FROM
 WHERE 
    CATALOG_NAME ='SAPERP.SE16'
    AND CUBE_NAME like  '%MBEW%'
+
+
+
+
+
+
+-----SQL query to check running statements/procedures in HANA memory and their memory consumption
+
+select
+c.host, c.user_name, c.connection_status, c.transaction_id, s.last_executed_time,
+round(s.allocated_memory_size/1024/1024/1024,2) as "Alloc Mem (GB)",
+round(s.used_memory_size/1024/1024/1024,2) as "Used Mem (GB)", s.statement_string
+from
+m_connections c, m_prepared_statements s
+where
+s.connection_id = c.connection_id and c.connection_status != 'IDLE'
+AND s.statement_string LIKE '%STABILIZATION%'
+order by
+s.allocated_memory_size desc
+;
+
